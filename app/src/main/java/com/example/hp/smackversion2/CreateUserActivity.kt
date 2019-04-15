b4784a01c3bccbfe06dec49e5b4c1df68892ed15
cthoughtz <cthoughtz@gmail.com>
2019-04-15 12:13:27 -0400

    created create user activity

---
+++ app/src/main/java/com/example/hp/smackversion2/CreateUserActivity.kt
@@ -0,0 +1,26 @@
+package com.example.hp.smackversion2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvator(view: View) {
+
+    }
+
+    fun generateColorClicked(view: View) {
+
+    }
+
+    fun createUserClicked(view: View) {
+
+    }
+
+}
--- app/src/main/java/com/example/hp/smackversion2/LoginActivity.kt
+++ app/src/main/java/com/example/hp/smackversion2/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.hp.smackversion2
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,7 +13,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginBtnClicked(view: View) {
-
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
     }
 
     fun loginCreatedUserBtnClicked(view: View) {
